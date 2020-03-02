f032fa5561bda26c3a2d3cf2f5d136bf8e09d265
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-02 22:03:51 +0100

    Create todo component

--- asp-react/ClientApp/src/App.js
+++ asp-react/ClientApp/src/App.js
@@ -7,9 +7,30 @@ import { Counter } from './components/Counter';
 
 import './custom.css'
 import AppFooter from './components/AppFooter';
+import Todos from './components/Todos';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
+
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                complete:false
+            },
+            {
+                id: 2,
+                title: 'Diner with wife',
+                complete: true
+            },
+            {
+                id: 3,
+                title: 'Meeting with boss',
+                complete: false
+            }
+        ]
+    }
 
   render () {
     return (
@@ -17,6 +38,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Todos todos={this.state.todos}/>
         <AppFooter />
       </Layout>
     );
